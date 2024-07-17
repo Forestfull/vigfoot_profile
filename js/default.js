32c08a87b62181fc9771a0bb0cacda9df3e6a792
Xasquatch <bigfoot7774@gmail.com>
2024-07-17 19:39:02 +0900

    - 타이핑 펑션 메모리 관리

--- js/default.js
+++ js/default.js
@@ -1,15 +1,23 @@
+const intervalAddr = {};
+
 function typeTextIntoNode(queryString, text) {
     let i = 0, timeout = 50;
     const nodes = document.querySelectorAll(queryString);
-    const fnTypingChar = char => setTimeout(() => {
+    const fnTypingChar = (char, address) => setTimeout(() => {
         nodes.forEach(node => {
             if (node.querySelectorAll(queryString).length > 0)
                 throw new DOMException('node안에 node존재');
 
-            node.innerHTML += text.charAt(i);
+            node.innerHTML += char;
+            clearInterval(address);
+            delete intervalAddr[address];
         });
-        if (i < text.length) fnTypingChar(text.charAt(i++));
+
+        if (i < text.length) {
+            let addr = Number(new Date());
+            intervalAddr[addr] = fnTypingChar(text.charAt(i++), addr);
+        }
     }, timeout);
 
-    fnTypingChar(text);
+    fnTypingChar(text.charAt(i++));
 }
