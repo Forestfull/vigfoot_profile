d7427dd1b876754ee1468632c4c31aca938d25e3
Xasquatch <bigfoot7774@gmail.com>
2024-07-22 19:23:49 +0900

    싱글태그 출력 작업 중 [이슈] - 첫 인덱스 씹힘 - 첫 인덱스 이중 출력

--- js/init.js
+++ js/init.js
@@ -1,33 +1,71 @@
 const intervalAddr = {};
 const intervalTimeout = 50;
-const singleTagList = ['br', 'hr', 'img', 'input'];
+const singleTagSet = new Set()
+    .add('br')
+    .add('hr')
+    .add('img')
+    .add('input');
 
 function text(queryString, text, interval, callback) {
     if (text === '' || text === null || text === undefined) return;
 
     let i = 0;
     const nodes = typeof queryString === 'object' ? [queryString] : document.querySelectorAll(queryString);
-    const fnTypingChar = (char, address) => setTimeout(() => {
-        nodes.forEach(node => {
-            if (typeof queryString !== 'object' && node.querySelectorAll(queryString).length > 0)
-                throw new DOMException('node안에 node존재');
 
-            node.innerHTML += char;
-            clearInterval(address);
-            delete intervalAddr[address];
-        });
+    function isSingleTag(txt) {
+        if (typeof txt !== 'string') return false;
+        return singleTagSet.has(txt.toLowerCase());
+    }
 
-        if (i < text.length) {
-            let addr = Number(new Date());
-            intervalAddr[addr] = fnTypingChar(text.charAt(i++), addr);
-        } else if (i === text.length && callback !== null && callback !== undefined){
-            callback();
-        }
+    function fnTypingChar(char, address) {
+        return setTimeout(() => {
+            if (text.charAt(i) === '<') {
+                const startTag = {};
+                startTag.endIdx = text.indexOf('>', i);
+                startTag.str = text.substring(i, startTag.endIdx + 1);
+                startTag.splitIdx = startTag.str.substring(i, startTag.endIdx).indexOf(' ');
 
-    }, interval === undefined || interval === null ? intervalTimeout : interval);
+                if (startTag.splitIdx !== -1) {
+                    startTag.name = startTag.str.substring(1, startTag.splitIdx);
+                    startTag.info = startTag.str.substring(startTag.splitIdx, startTag.endIdx);
 
-    fnTypingChar(text.charAt(i++));
+                } else {
+                    startTag.name = startTag.str.substring(1, startTag.endIdx - i);
+                }
+                if (isSingleTag(startTag.name)) {
+                    i += startTag.str.length + 1;
+                    nodes.forEach(node => node.innerHTML += startTag.str);
 
+                }
+                console.log(startTag)
+
+            } else {
+                nodes.forEach(node => {
+                    if (typeof queryString !== 'object' && node.querySelectorAll(queryString).length > 0)
+                        throw new DOMException('node안에 node존재');
+
+                    node.innerHTML += char;
+                    clearInterval(address);
+                    delete intervalAddr[address];
+                });
+
+            }
+
+            if (i < text.length) {
+                let addr = Number(new Date());
+                intervalAddr[addr] = fnTypingChar(text.charAt(i), addr);
+
+            } else if (i === text.length && callback !== null && callback !== undefined) {
+                callback();
+
+            }
+
+            i++;
+
+        }, interval === undefined || interval === null ? intervalTimeout : interval);
+    }
+
+    fnTypingChar(text.charAt(i), null);
 }
 
 const loading = {
